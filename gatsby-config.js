f7c1e04f6ee6809d74046b90d1ff769ad0a8511b
Mark Creasy <mcreasy50@gmail.com>
2022-06-17 10:04:41 -0400

    gatsby init

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: `Linda Benson Interiors`,
+    siteUrl: `https://www.lindabensoninteriors.com`,
+  },
+  plugins: [
+    {
+    //   resolve: `gatsby-source-mysql`,
+    //   options: {
+    //     connectionDetails: {
+    //       host: 'localhost',
+    //       user: 'db-username',
+    //       password: 'db-password',
+    //       database: 'world'
+    //     },
+    //     queries: [
+    //       {
+    //         statement: 'SELECT * FROM country',
+    //         idFieldName: 'Code',
+    //         name: 'country'
+    //       }
+    //     ]
+    //   }
+    // }
+  ],
+}
