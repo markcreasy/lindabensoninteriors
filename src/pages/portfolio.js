83dd9fa02f892ae6c472fc2246978de3d2cc8640
Mark Creasy <mcreasy50@gmail.com>
2022-06-17 14:59:50 -0400

    update image file structure

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -5,36 +5,46 @@ import { StaticImage } from "gatsby-plugin-image"
 
 const Portfolio = ({ data }) => {
 
-  const projects = {
-    "asburyBath":[],
-    "asburyOffice":[],
-    "barclayPoolhouse":[],
-    "gentlemensBar":[],
-    "MansionInMay":[],
-    "mim2015":[],
-    "mulheren":[],
-    "rumson":[],
-    "statelyHomes":[],
-    "townGym":[],
-    "townPowder":[],
-    "townRetreat":[],
-    "weekendOffice":[],
-    "youngLadies":[]
+  const projectNames = [
+    {dir:"mim2015",name:"Mansion in May 2014"},
+    {dir:"statelyHomes",name:"Stately Homes by the Sea 2013"},
+    {dir:"MansionInMay",name:"Mansion in May 2012"},
+    {dir:"townRetreat",name:"Hunterdon Retreat"},
+    {dir:"rumson",name:"Rumson Family Room"},
+    {dir:"rumsonHallway",name:"Rumson Hallway"},
+    {dir:"rumsonGuest",name:"Rumson Guest Room 1"},
+    {dir:"rumsonBed",name:"Rumson Guest Room 2"},
+    {dir:"barclayPoolhouse",name:"Tewksbury Poolhouse"},
+    {dir:"powderRoom",name:"Bathrooms/Powder Rooms"},
+    {dir:"asburyOffice",name:"Asbury Office"},
+    {dir:"weekendOffice",name:"Weekend Office"},
+    {dir:"gentlemensBar",name:"Gentelmen's Bar"},
+    {dir:"townGym",name:"Oldwick Gym"}
+  ]
+
+  const projects = projectNames.map( (project) => {
+    return {
+      name: project.name,
+      photos: data.allFile.nodes.filter( img => img.relativeDirectory === project.dir).map( img => {return img.relativePath})
+    }
+  });
+
+  function ProductList(){
+    const listItems = projects.map((project) =>
+      <li key={project.name}>{project.name}</li>
+    );
+    return (
+      <ul>{listItems}</ul>
+    )
   }
 
-  const projectPhotos = data.allFile.nodes.map( node => {
-    if(projects[node.relativeDirectory]) projects[node.relativeDirectory].push(node.relativePath);
-  });
-  // const projects = data.allFile.nodes.group(({relativeDirectory}) => relativeDirectory);
-  console.log("projects: ", projects);
 
   return (
     <Layout pageTitle="porfolio">
     <div id="contentWrapper">
       <div id="projectsArea">
           <h3 id="picTitle">Projects</h3>
-          
-
+          <ProductList />
       </div>
       </div>
     </Layout>
